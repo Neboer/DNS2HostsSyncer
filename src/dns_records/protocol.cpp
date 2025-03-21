a0c7c61375600bd87bf0222560066a8d7a92dc80
Neboer <rubinposter@gmail.com>
2025-03-21 09:03:53 +0800

    修复fqdn域名带 . 对CNAME解析造成的影响。

--- src/dns_records/protocol.cpp
+++ src/dns_records/protocol.cpp
@@ -2,33 +2,41 @@
 #include "record.h"
 #include <spdlog/spdlog.h>
 
+str remove_fqdn(const str &domain_with_fqdn_dot)
+{
+    if (domain_with_fqdn_dot.back() == '.')
+    {
+        return domain_with_fqdn_dot.substr(0, domain_with_fqdn_dot.size() - 1);
+    }
+    else
+    {
+        return domain_with_fqdn_dot;
+    }
+}
+
 namespace d2hs
 {
-    str remove_fqdn(const str &domain_with_fqdn_dot)
-    {
-        if (domain_with_fqdn_dot.back() == '.')
-        {
-            return domain_with_fqdn_dot.substr(0, domain_with_fqdn_dot.size() - 1);
-        }
-        else
-        {
-            return domain_with_fqdn_dot;
-        }
-    }
-
     std::vector<RRRecord> parse_pdns_response(const json &response)
     {
         std::vector<RRRecord> output_records;
 
         for (const auto &rr_record : response["rrsets"])
         {
+            const RecordType rt = str_to_record_type(rr_record["type"]);
+            str dot_free_value = rr_record["records"][0]["content"];
+            if (rt == RecordType::CNAME)
+            {
+                // CNAME记录的content是一个FQDN，我们需要移除末尾的“.”。
+                dot_free_value = remove_fqdn(dot_free_value);
+            }
+
             try
             {
                 output_records.push_back(
                     {
                         remove_fqdn(rr_record["name"]),
-                        rr_record["records"][0]["content"],
-                        str_to_record_type(rr_record["type"])    
+                        dot_free_value,
+                        rt    
                     });
             }
             catch (const std::invalid_argument &e)
